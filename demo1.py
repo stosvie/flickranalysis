a636ae0f60e1c0cea7a05f48f317499d32fabd3b
stosvie <37210621+stosvie@users.noreply.github.com>
2020-02-14 19:39:56 +0100

    Cleanup

--- demo1.py
+++ demo1.py
@@ -1,4 +1,7 @@
+import datetime
+from datetime import date, timedelta
 import time
+
 from urllib import parse
 
 import flickrapi
@@ -6,13 +9,8 @@ import flickrapi
 import pandas as pd
 import sqlalchemy as sa
 import webbrowser
-import datetime
-from datetime import date, timedelta
-import time
 
-
-
-def calldb(data,table_name):
+def calldb(data, table_name):
 
     connecting_string = 'Driver={ODBC Driver 17 for SQL Server};Server=tcp:woo.database.windows.net,1433;Database=BYWS;Uid=boss;Pwd=s7#3QzOsB$J*^v3;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
     params = parse.quote_plus(connecting_string)
@@ -37,8 +35,7 @@ def get_saved_favs(pid):
 
     ndf = df[df['favedate'] > str(1498608604)]
 
-
-def get_photo_batch(fobj,userid,page_to_get):
+def get_photo_batch(fobj, userid, page_to_get):
     # photos = fobj.photos.search(user_id=userid, per_page='30', extras='views')
     photos = flickr2.people.getPhotos(user_id=myuserid, page=page_to_get)
 
@@ -105,52 +102,68 @@ def stat_helper(dt,pg):
     for x in popular['photos']['photo']:
         df_stats = df_stats.append(x['stats'], ignore_index=1)
     df_final = df_final.join(df_stats)
-    return ( df_final, popular['photos']['pages'] , popular['photos']['page'])
+    return df_final, popular['photos']['pages'] , popular['photos']['page']
 
-def getStats():
-    #dt =
+def get_stats():
     datelist = pd.date_range(date.today() - timedelta(29), periods=30).tolist()
     for d in datelist:
         ts = d.value
         dt = datetime.datetime(d.year, d.month, d.day)
-        res = stat_helper(dt,1)
+        res = stat_helper(dt, 1)
         final_df = res[0]
-        # popular = flickr2.stats.getPopularPhotos(date=dt, per_page=100, page=1)
-        # df_popular = pd.DataFrame(popular['photos']['photo'])
-        # df_id = df_popular[['id', 'title']]
-        # #df_stats = pd.DataFrame(df_popular['stats'])
-        # #df_s = pd.DataFrame(df_stats['stats'].values)
-        # fd_# f = pd.DataFrame()
-        # for x in popular['photos']['photo']:
-        #     fd_f = fd_f.append(x['stats'], ignore_index=1)
-        # df_id = df_id.join(fd_f)
+
         while res[1] - res[2] > 0:
             res = stat_helper(dt, res[2]+1)
             final_df = final_df.append(res[0])
 
-        # while popular['photos']['pages'] - popular['photos']['page'] > 0:
-        #     popular = flickr2.stats.getPopularPhotos(date=dt, per_page=100, page=popular['photos']['page'] + 1)
-        #     df_popular = pd.DataFrame(popular['photos']['photo'])
-        #     # df_id = df_popular[['id', 'title']]
-        #     for x in popular['photos']['photo']:
-        #         fd_f = fd_f.append(x['stats'], ignore_index=1)
-        #     df_id = df_id.join(fd_f)
-        #     df_id = df_id.append(df_next)
 
         dt_list = [dt for i in range(final_df.index.size)]
         final_df['statdate'] = dt_list
-        # write off to db
+
         calldb(final_df, 'photo_stats')
         print('nxt date')
 
     print('Done')
 
+def tests():
+    #sets = flickr2.photosets.getList(user_id='58051209@N00')
+    photos = flickr2.people.getPhotos(user_id=myuserid, page=1)
+    # dt = datetime.datetime(2020, 2, 14, 10, 00)
+    # x = time.mktime(dt.timetuple())
+    # min_date = flickr2.photos.recentlyUpdated(min_date=x)
+    # try:
+    #    popular = flickr2.stats.getPopularPhotos(date='2019-02-13', per_page=100)
+    # except flickrapi.exceptions.FlickrError:
+    #    print(flickrapi.exceptions.FlickrError)
+    # allfavs = get_all_favs(flickr2,  30507209051)
+
+    # zz = get_saved_favs(30507209051)
+    for f in photos['photos']['photo']:
+        photoid = f['id']
+
+        phototitle = f['title']
+        i1 = flickr2.photos.getInfo(photo_id=photoid, format='parsed-json')
+        exif = flickr2.photos.getExif(photo_id=photoid)
+        groups = flickr2.photos.getAllContexts(photo_id=photoid)
+        stats = flickr2.stats.getTotalViews(date='2020-01-31')
+
+    # getStats()
+
+    # info = flickr2.photos.getInfo(photo_id=f'49506034036', format='parsed-json')
+
+    # set = flickr2.walk_set('72157703860493334', per_page=20)
+    # for photo in set[1:4]:
+    #     print ('Hello')
+
+
+### These need to be moved to the future flickrana class
 api_key = u'4a69280a31fc96c26e7d218c3a8cf345'
 api_secret = u'a2d9a639fd955497'
-
-api_key2 = u'ec9da859df80dec922bac6c9104c1f0f'
-api_secret2 = u'ee4603144f00eba9'
 myuserid = u'58051209@N00'
+
+#api_key2 = u'ec9da859df80dec922bac6c9104c1f0f'
+#api_secret2 = u'ee4603144f00eba9'
+
 
 flickr2 = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
 print('Step 1: authenticate')
@@ -174,36 +187,6 @@ if not flickr2.token_valid(perms='read'):
     # Trade the request token for an access token
     flickr2.get_access_token(verifier)
 
-
-
-
-
-# flickr = flickrapi.FlickrAPI(api_key2, api_secret2)
-# flickr2 = flickrapi.FlickrAPI(api_key, api_secret, format='etree')
-
-
-
-# raw_json = flickr2.photosets.getList(user_id='58051209@N00')
-# raw_json -> '{...}'
-#
-#res = flickr2.people.getPhotos(user_id=myuserid)
-# parsed = json.loads(raw_json.decode('utf-8'))
-
-sets = flickr2.photosets.getList(user_id='58051209@N00')
-photos = flickr2.people.getPhotos(user_id=myuserid, page=1)
-#dt = datetime.datetime(2020, 2, 14, 10, 00)
-#x = time.mktime(dt.timetuple())
-#min_date = flickr2.photos.recentlyUpdated(min_date=x)
-#try:
-#    popular = flickr2.stats.getPopularPhotos(date='2019-02-13', per_page=100)
-#except flickrapi.exceptions.FlickrError:
-#    print(flickrapi.exceptions.FlickrError)
-#allfavs = get_all_favs(flickr2,  30507209051)
-
-
-#zz = get_saved_favs(30507209051)
-getStats()
-
 # TODO: from getInf
 #       dates,comments views
 #       save date of lst update get recentlyUpdated with photos.recentlyUpdated
@@ -212,28 +195,8 @@ getStats()
 #       groups from getAllContexts
 #       * recentlyUpdate also need oAuth
 
-for f in photos['photos']['photo']:
-    photoid = f['id']
+print('Done.')
 
-    phototitle = f['title']
-    i1 = flickr2.photos.getInfo(photo_id=photoid, format='parsed-json')
-    exif = flickr2.photos.getExif(photo_id=photoid)
-    groups = flickr2.photos.getAllContexts(photo_id=photoid)
-    stats = flickr2.stats.getTotalViews(date='2020-01-31')
-print ('Done.')
-
-# infojson  = json.load(info)
-# with open('T:/@dev/python/flickr/photoinfo.json', 'w') as outfile:
-#    json.dump(infojson, outfile)
-
-# info = flickr2.photos.getInfo(photo_id=f'49506034036', format='parsed-json')
-
-
-
-
-# set = flickr2.walk_set('72157703860493334', per_page=20)
-# for photo in set[1:4]:
-#     print ('Hello')
 
 
 
